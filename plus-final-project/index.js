ec19f3cc760468cf63f5ff992152fd9a3a4bcd3b
Tash2911 <160403113+Tash2911@users.noreply.github.com>
2024-06-03 21:12:15 +0200

    Added APIs

--- plus-final-project/index.js
+++ plus-final-project/index.js
@@ -39,7 +39,6 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "5804cet5d8681bbaa2c0f31803d6o3f5";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}& units=metric or imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
